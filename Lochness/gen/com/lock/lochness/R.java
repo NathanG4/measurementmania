745a599c973bfdd6c1faf67c95f342672ae9712d
Nathan Garcia <s291292@SPHA307M15.ocsb.local>
2013-12-10 11:12:47 -0500

    Added a table layout

--- Lochness/gen/com/lock/lochness/R.java
+++ Lochness/gen/com/lock/lochness/R.java
@@ -28,12 +28,18 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090000;
-        public static final int action_settings=0x7f090006;
-        public static final int button1=0x7f090002;
-        public static final int button2=0x7f090004;
-        public static final int button3=0x7f090001;
+        public static final int action_settings=0x7f09000c;
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090007;
+        public static final int button3=0x7f090006;
         public static final int button4=0x7f090003;
-        public static final int textView1=0x7f090005;
+        public static final int tableLayout1=0x7f090001;
+        public static final int tableRow1=0x7f090002;
+        public static final int tableRow2=0x7f090005;
+        public static final int tableRow3=0x7f090008;
+        public static final int tableRow4=0x7f090009;
+        public static final int textView1=0x7f09000b;
+        public static final int textView2=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -48,6 +54,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int ar=0x7f050008;
         public static final int intro=0x7f050007;
+        public static final int introoo=0x7f050009;
         public static final int p=0x7f050004;
         public static final int sa=0x7f050005;
         public static final int v=0x7f050006;
